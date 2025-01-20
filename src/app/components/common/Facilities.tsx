8f60e2c02717e21fc38553c6df7fdd7d55d13bf7
Joseph <kamoldinovq@gmail.com>
2025-01-21 08:04:50 +0900

    feat: develop facilites page

--- src/app/components/common/Facilities.tsx
+++ src/app/components/common/Facilities.tsx
@@ -1,4 +1,26 @@
-import React from "react"
-export default function Facilities () {
-   return  <div>Facilities</div>
+import { Container, Stack } from "@mui/material";
+import React from "react";
+export default function Facilities() {
+  return (
+    <Stack className="facilities">
+      <Container className="frame">
+        <Stack className="card">
+          <span className="strong">Fast Delivery</span>
+          <span className="span">Delivery within 24 Hours in any Place</span>
+        </Stack>
+        <Stack className="card">
+          <span className="strong">24/7 Support</span>
+          <span className="span">Get our support any time at any hour</span>
+        </Stack>
+        <Stack className="card">
+          <span className="strong">Secure Shopping</span>
+          <span className="span">Highly secured online payment method</span>
+        </Stack>
+        <Stack className="card">
+          <span className="strong">Moneyback</span>
+          <span className="span">Guaranteed money back in 30 days</span>
+        </Stack>
+      </Container>
+    </Stack>
+  );
 }
