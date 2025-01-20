f20d92e875bc9388e8e1a82a33bc282e5f89c3ac
Joseph <kamoldinovq@gmail.com>
2025-01-21 07:20:09 +0900

    feat: develop advertisement section

--- src/app/screens/homePage/Advertisement.tsx
+++ src/app/screens/homePage/Advertisement.tsx
@@ -1,4 +1,8 @@
 import React from "react"
 export default function Advertisement() {
-  return <div>Advertisement</div>;
+  return <div className="ads-shop-frame">
+    <video className="ads-video" autoPlay={true} loop muted playsInline data-video-media="">
+      <source type="video/mp4" src="video/video2.mp4" />
+    </video>
+  </div>;
 }
